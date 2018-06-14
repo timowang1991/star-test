a556450ebaf79db447f6a264c73b42206ef40166
Timo Wang <chiuanw@yahoo-inc.com>
2018-06-14 15:00:47 +0800

    update to build/ path

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var path = require('path');
 
 exports.get = function(event, context, callback) {
-  var contents = fs.readFileSync(`public${path.sep}index.html`);
+  var contents = fs.readFileSync(`build${path.sep}index.html`);
   var result = {
     statusCode: 200,
     body: contents.toString(),
